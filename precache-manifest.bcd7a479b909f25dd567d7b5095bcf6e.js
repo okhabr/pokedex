4e68e2561263f42729d9e5702c7a422f2e81ced8
Oksana Khabrovska <o.khabrovska@gmail.com>
2020-05-21 18:42:41 +0400

    Updates

---
+++ precache-manifest.bcd7a479b909f25dd567d7b5095bcf6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c669a654f01f0c2a7061e08b5df08",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "de5668c641ef1a91aa2c",
+    "url": "/pokedex/static/css/main.9e674bfc.chunk.css"
+  },
+  {
+    "revision": "b8f87322e2e0e8c63fc6",
+    "url": "/pokedex/static/js/2.f7e4538b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.f7e4538b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5668c641ef1a91aa2c",
+    "url": "/pokedex/static/js/main.5692abf7.chunk.js"
+  },
+  {
+    "revision": "0d9466e3f782831f3e87",
+    "url": "/pokedex/static/js/runtime-main.2b038e16.js"
+  }
+]);
